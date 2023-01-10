f3f51b4fb3234107da5c7957c61a08b14409a13c
unknown <susalafamilia@gmail.com>
2023-01-10 21:53:10 +0200

    + some styling

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,32 +1,38 @@
-import React, {useEffect, useState} from 'react';
-import {connect} from "react-redux";
-import {selectProducts} from "../../redux/selectors/products-selectors";
-import {fetchProducts} from "../../redux/reducers/products-reducer";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { selectProducts } from "../../redux/selectors/products-selectors";
+import { fetchProducts } from "../../redux/reducers/products-reducer";
 import ProductCard from "../ProductCard/ProductCard";
-import mainPageStyles from './MainPage.module.css'
+import mainPageStyles from "./MainPage.module.css";
 
 function MainPage(props) {
-    useEffect(props.fetchProducts, [])
-    console.log(props)
+  useEffect(props.fetchProducts, []);
+  console.log(props);
 
-    let productsTemplateArray = props.products.map(product => <ProductCard key={product.id} title={product.title}
-                                                                           price={product.price}
-                                                                           category={product.category}
-                                                                           description={product.description}
-                                                                           image={product.image}/>)
-    return (
-        <div>
-            Products
-            <div className={mainPageStyles.products}></div>
-            {productsTemplateArray}
-        </div>
-    );
+  let productsTemplateArray = props.products.map((product) => (
+    <ProductCard
+      key={product.id}
+      title={product.title}
+      price={product.price}
+      category={product.category}
+      description={product.description}
+      image={product.image}
+    />
+  ));
+  return (
+    <div>
+      <div className={mainPageStyles.container}>
+        Products
+        <div className={mainPageStyles.products}>{productsTemplateArray}</div>
+      </div>
+    </div>
+  );
 }
 
 let mapStateToProps = (state) => {
-    return {
-        products: selectProducts(state)
-    }
-}
+  return {
+    products: selectProducts(state),
+  };
+};
 
-export default connect(mapStateToProps, {fetchProducts})(MainPage);
+export default connect(mapStateToProps, { fetchProducts })(MainPage);
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import productCardStyles from './ProductCard.module.css'
+import React from "react";
+import productCardStyles from "./ProductCard.module.css";
 
-function ProductCard({title, price, category, description, image}) {
-    return (
-        <div className={productCardStyles.productCard}>
-            <img src={image}/>
-            <h4>{title}</h4>
-            <p>{category}</p>
-            <p>{description}</p>
-            <p>{price}</p>
-        </div>
-    );
+function ProductCard({ title, price, category, description, image }) {
+  return (
+    <div className={productCardStyles.productCard}>
+      <div className={productCardStyles.imageContainer}>
+        <img src={image} />
+      </div>
+      <div className={productCardStyles.textInfo}>
+        <h4>{title}</h4>
+        <p>{category}</p>
+        <p>{price} $</p>
+      </div>
+    </div>
+  );
 }
 
 export default ProductCard;
